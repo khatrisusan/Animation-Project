a3fc2057b47c5ae39197ea8cb5cd87314905219e
Susan <khatri.susan123@gmail.com>
2020-03-23 11:20:07 +0100

    fetched json

--- animation_project.js
+++ animation_project.js
@@ -2,4 +2,13 @@
 window.addEventListener("DOMContentLoaded", init);
 function init() {
   console.log("ready");
+  fetchTimeline();
+}
+async function fetchTimeline() {
+  const response = await fetch("timeline.json");
+  const jsonData = await response.json();
+  prepareData(jsonData);
+}
+function prepareData(data) {
+  console.log(data);
 }
