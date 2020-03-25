d6ecbf042989ba0acf34be5e3d057431cc4f7c55
Juga limbu <labungjugal1@gmail.com>
2020-03-25 21:48:00 +0100

    fetched the image json

--- main.js
+++ main.js
@@ -5,18 +5,25 @@ function init() {
   console.log("ready");
   fetchTimeline();
   fetchSVG();
+  fetchImage();
 }
 async function fetchTimeline() {
   const response = await fetch("timeline.json");
   const jsonData = await response.json();
   prepareData(jsonData);
 }
-function prepareData(data) {
+function prepareData(data, myImage) {
   console.log(data);
+  console.log(myImage);
 }
 async function fetchSVG() {
   let response = await fetch("tree.svg");
   let mySvgData = await response.text();
   document.querySelector("#treeSVG").innerHTML = mySvgData;
   //startManipulatingSvg();
+}
+async function fetchImage() {
+  let response = await fetch("image.json");
+  let myImage = await response.json();
+  prepareData(myImage);
 }
